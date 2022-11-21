2966c8951ccf220fe36af60fd5c3ce3ffcc4e307
Manan <manan228@yahoo.in>
2022-11-21 16:46:29 +0530

    connection to mongodb and starting off

---
+++ models/product.js
@@ -0,0 +1,48 @@
+// const Sequelize = require('sequelize');
+
+// const sequelize = require('../util/database');
+
+const getDb = require("../util/database").getDb;
+
+class Product {
+  constructor(title, price, description, imageUrl) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+  }
+
+  save() {
+    console.log(`inside save`)
+    const db = getDb();
+    return db
+      .collection("products")
+      .insertOne(this)
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
+  }
+}
+
+// const Product = sequelize.define("product", {
+//   id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     allowNull: false,
+//     primaryKey: true,
+//   },
+//   title: Sequelize.STRING,
+//   price: {
+//     type: Sequelize.DOUBLE,
+//     allowNull: false,
+//   },
+//   imageUrl: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//   },
+//   description: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//   },
+// });
+
+module.exports = Product;
---
+++ util/database.js
@@ -0,0 +1,39 @@
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://manan:2NfEKvhac9Mr5zhA@cluster0.17cyjfi.mongodb.net/shop?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("Connected!");
+      _db = client.db();
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+
+  throw "No database found!";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
+
+// const Sequelize = require('sequelize');
+
+// const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+//   dialect: 'mysql',
+//   host: 'localhost'
+// });
+
+// module.exports = sequelize;
