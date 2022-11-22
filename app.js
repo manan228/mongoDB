9305af1150987f3a1dda3748ff4973eb62f0423b
Manan <manan228@yahoo.in>
2022-11-22 16:38:16 +0530

    added relation, add to cart logic

--- app.js
+++ app.js
@@ -28,11 +28,10 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findById("637c9dd5a9fed1834a76c5b8")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
-  next();
 });
 
 app.use("/admin", adminRoutes);
